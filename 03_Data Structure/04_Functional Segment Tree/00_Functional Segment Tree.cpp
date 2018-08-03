c8c0be2392a9f67fd4881f6763f44d1f2a964dc0
superkun <superkunn@users.noreply.github.com>
2018-08-03 15:05:59 +0800

    Update 00_Functional Segment Tree.cpp

--- 03_Data Structure/04_Functional Segment Tree/00_Functional Segment Tree.cpp
+++ 03_Data Structure/04_Functional Segment Tree/00_Functional Segment Tree.cpp
@@ -2,25 +2,62 @@
 // 静态查询区间第$k$小的值\\
 // 必要时进行离散化
 // ---
-const int maxn = "Edit";
-int a[maxn], rt[maxn];
-int cnt;
-int lson[maxn << 5], rson[maxn << 5], sum[maxn << 5];
-#define Lson l, m, lson[x], lson[y]
-#define Rson m + 1, r, rson[x], rson[y]
-void update(int p, int l, int r, int& x, int y)
-{
-    lson[++cnt] = lson[y], rson[cnt] = rson[y], sum[cnt] = sum[y] + 1, x = cnt;
-    if (l == r) return;
-    int m = (l + r) >> 1;
-    if (p <= m) update(p, Lson);
-    else update(p, Rson);
+//poj 2104
+#include<cstdio>
+#include<iostream>
+#include<cmath>
+#include<queue>
+#include<stack>
+#include<set>
+#include<map>
+#include<algorithm>
+#include<vector>
+#include<string>
+#include<cstring>
+using namespace std;
+#define rep(i,a,b) for(int i=a;i<=b;i++)
+#define per(i,a,b) for(int i=a;i>=b;i--)
+#define pb push_back
+#define mp make_pair
+#define all(x) (x).begin(),(x).end()
+typedef long long ll;
+typedef vector<int> vi;
+typedef pair<int,int> pii;
+const int MAXN=1e5+6;
+int n,m,cnt,x,y,k,root[MAXN],a[MAXN];
+struct node{int l,r,sum;}T[MAXN*40];
+vi v;
+int getid(int x){return lower_bound(all(v),x)-v.begin()+1;}
+void update(int l,int r,int &x,int y,int pos){
+    x=++cnt;
+    T[x]=T[y];
+    T[x].sum++;
+    if(l==r)return;
+    int mid=(l+r)>>1;
+    if(mid>=pos)update(l,mid,T[x].l,T[y].l,pos);
+    else update(mid+1,r,T[x].r,T[y].r,pos);
 }
-int query(int l, int r, int x, int y, int k)
-{
-    if (l == r) return l;
-    int m = (l + r) >> 1;
-    int s = sum[lson[y]] - sum[lson[x]];
-    if (s >= k) return query(Lson, k);
-    else return query(Rson, k - s);
+int query(int l,int r,int x,int y,int k){
+    if(l==r)return l;
+    int sum=T[T[y].l].sum-T[T[x].l].sum;
+    int mid=(l+r)>>1;
+    if(sum>=k)return query(l,mid,T[x].l,T[y].l,k);
+    else return query(mid+1,r,T[x].r,T[y].r,k-sum);
+}
+int work(){
+    scanf("%d%d",&n,&m);
+    v.clear();
+    rep(i,1,n)scanf("%d",&a[i]),v.pb(a[i]);
+    sort(all(v)),v.erase(unique(all(v)),v.end());
+    cnt=0;
+    rep(i,1,n)update(1,n,root[i],root[i-1],getid(a[i]));
+    rep(i,1,m)scanf("%d%d%d",&x,&y,&k),printf("%d\n",v[query(1,n,root[x-1],root[y],k)-1]);
+    return 0;
+}
+int main(){
+#ifdef superkunn
+    freopen("input.txt","rt",stdin);
+#endif
+    work();
+    return 0;
 }
