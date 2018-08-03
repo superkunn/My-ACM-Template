ed9c46a1ea0a4acfbb1e8c62fe32fe2328b05225
superkun <superkunn@users.noreply.github.com>
2018-08-03 14:42:41 +0800

    Update 00_Union-Find-Set.cpp

--- 04_Graph Theory/01_Union-Find Set/00_Union-Find-Set.cpp
+++ 04_Graph Theory/01_Union-Find Set/00_Union-Find-Set.cpp
@@ -1,21 +1,8 @@
-const int maxn = "Edit";
-int n, fa[maxn], ra[maxn];
-void init()
-{
-    clr(ra, 0);
-    iota(fa, fa + n, 0);
-}
-int find(int x) { return fa[x] != x ? fa[x] = find(fa[x]) : x; }
-void unite(int x, int y)
-{
-    x = find(x), y = find(y);
-    if (x == y) return;
-    if (ra[x] < ra[y])
-        fa[x] = y;
-    else
-    {
-        fa[y] = x;
-        if (ra[x] == ra[y]) ra[x]++;
-    }
-}
-bool same(int x, int y) { return find(x) == find(y); }
+const int MAXN=1e6+5;
+struct DSU{
+    int p[MAXN];
+    void init(int n){for(int i=0;i<=n;i++)p[i]=i;}
+    int findp(int x){return x==p[x]?x:p[x]=findp(p[x]);}
+    void unite(int x,int y){x=findp(x);y=findp(y);if(x==y)return;p[y]=x;}
+    bool same(int x,int y){return findp(x)==findp(y);}
+}dsu;
