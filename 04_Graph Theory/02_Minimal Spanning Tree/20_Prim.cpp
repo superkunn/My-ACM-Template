865e4841f73b96c975fbbfc9e0dd82a90c097b62
superkun <superkunn@users.noreply.github.com>
2018-08-03 14:45:55 +0800

    Update 20_Prim.cpp

--- 04_Graph Theory/02_Minimal Spanning Tree/20_Prim.cpp
+++ 04_Graph Theory/02_Minimal Spanning Tree/20_Prim.cpp
@@ -1,31 +1,49 @@
-// 耗费矩阵cost[][],标号从0开始,0~n-1
-// 返回最小生成树的权值,返回-1表示原图不连通
-const int maxn = "Edit";
-bool vis[maxn];
-int lowc[maxn];
-int Prim(int cost[][maxn], int n)
-{
-    int ans = 0;
-    clr(vis, 0);
-    vis[0] = 1;
-    for (int i = 1; i < n; i++)
-        lowc[i] = cost[0][i];
-    for (int i = 1; i < n; i++)
-    {
-        int minc = INF;
-        int p = -1;
-        for (int j = 0; j < n; j++)
-            if (!vis[j] && minc > lowc[j])
-            {
-                minc = lowc[j];
-                p = j;
-            }
-        if (minc == INF) return -1;
-        vis[p] = 1;
-        ans += minc;
-        for (int j = 0; j < n; j++)
-            if (!vis[j] && lowc[j] > cost[p][j])
-                lowc[j] = cost[p][j];
-    }
-    return ans;
+#include<bits/stdc++.h>
+using namespace std;
+const int INF=1e9+5;
+const int MAXE=1e5+5;
+const int MAXV=3e5+5;
+typedef pair<int,int> P;
+int V,E;
+struct edge{
+	int to,cost;
+};
+vector<edge> G[MAXV];// adjacency table
+priority_queue<P, vector<P>, greater<P> > pq; //priority queue(defaulted to extract the minimum)
+bool used[MAXV];// to judge if some vertex is in the MST
+int dis[MAXV];//minimum distance to vertex i
+int prim(){
+	int ans=0;
+	memset(used,false,sizeof(used));
+	fill(dis,dis+V,INF);
+	dis[0]=0;
+	while(!pq.empty()) pq.pop();
+	pq.push(P(0,0));
+	while(!pq.empty()){
+		P p=pq.top();pq.pop();
+		int v=p.second;
+		if(used[v]||p.first>dis[v]) continue;
+		used[v]=true;
+		ans+=dis[v];
+		for(int i=0;i<G[v].size();i++){
+			edge e=G[v][i];
+			if(dis[e.to]>e.cost){
+				dis[e.to]=e.cost;
+				pq.push(P(dis[e.to],e.to));
+			}
+		}
+	}
+	return ans;
+}
+int main(){
+	scanf("%d%d",&V,&E);
+	for(int i=0;i<E;i++){
+		int x,y,z;
+		scanf("%d%d%d",&x,&y,&z);
+		x--,y--;//convert into 0-based
+		G[x].push_back((edge){y,z});
+		G[y].push_back((edge){x,z});
+	}
+	printf("%d\n",prim());
+	return 0;
 }
