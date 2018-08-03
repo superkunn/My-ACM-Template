22c04dff03b3c6a266417ece1ef0fa8d5c22b6de
superkun <superkunn@users.noreply.github.com>
2018-08-03 14:44:47 +0800

    Update 10_Kruskal.cpp

--- 04_Graph Theory/02_Minimal Spanning Tree/10_Kruskal.cpp
+++ 04_Graph Theory/02_Minimal Spanning Tree/10_Kruskal.cpp
@@ -1,22 +1,46 @@
-typedef pair<int, PII> Edge;
-vector<Edge> G;
-void add_edge(int u, int v, int d) { G.pb(mp(d, mp(u, v))); }
-int Kruskal(int n)
-{
-    init(n); // 并查集初始化
-    sort(G.begin(), G.end());
-    int num = 0, ret = 0;
-    for (auto& e : G)
-    {
-        int x = e.Y.X, y = e.Y.Y;
-        int d = e.X;
-        if (!same(x, y))
-        {
-            unite(x, y);
-            num++;
-            ret += d;
+//poj 1258
+#include<cstdio>
+#include<algorithm>
+using namespace std;
+const int MAXE=1e5+5;
+const int MAXN=1e5+5;
+struct DSU{
+    int p[MAXN];
+    void init(int n){for(int i=0;i<=n;i++)p[i]=i;}
+    int findp(int x){return x==p[x]?x:p[x]=findp(p[x]);}
+    void unite(int x,int y){x=findp(x);y=findp(y);if(x==y)return;p[y]=x;}
+    bool same(int x,int y){return findp(x)==findp(y);}
+}dsu;
+struct edge{int u,v,cost;}es[MAXE];
+bool cmp(const edge &x,const edge &y){return x.cost<y.cost;}
+int V,E;
+int kruskal(){
+    sort(es,es+E,cmp);
+    dsu.init(V);
+    int res=0;
+    for(int i=0;i<E;i++){
+        if(!dsu.same(es[i].u,es[i].v)){
+            dsu.unite(es[i].u,es[i].v);
+            res+=es[i].cost;
         }
-        if (num == n - 1) break;
     }
-    return ret;
+    return res;
+}
+int main(){
+    while(~scanf("%d",&V)){
+        E=0;
+        for(int i=1;i<=V;i++){
+            for(int j=1;j<=V;j++){
+                int w;
+                scanf("%d",&w);
+                if(i==j)continue;
+                es[E].u=i;
+                es[E].v=j;
+                es[E].cost=w;
+                E++;
+            }
+        }
+        printf("%d\n",kruskal());
+    }
+    return 0;
 }
