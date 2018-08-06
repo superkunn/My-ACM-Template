634729ba8f7441338136da2c7c99e3e15572d014
superkun <superkunn@users.noreply.github.com>
2018-08-06 23:33:55 +0800

    Update 01_High-precision.cpp

--- 07_Others/04_BigNum/01_High-precision.cpp
+++ 07_Others/04_BigNum/01_High-precision.cpp
@@ -1,26 +1,21 @@
-// 加法 乘法 小于号 输出
-struct bint
-{
+// < + * print
+struct bint{
     int l;
     short int w[100];
-    bint(int x = 0)
-    {
+    bint(int x = 0){
         l = x == 0, clr(w, 0);
         while (x) w[l++] = x % 10, x /= 10;
     }
-    bool operator<(const bint& x) const
-    {
+    bool operator<(const bint& x) const{
         if (l != x.l) return l < x.l;
         int i = l - 1;
         while (i >= 0 && w[i] == x.w[i]) i--;
         return (i >= 0 && w[i] < x.w[i]);
     }
-    bint operator+(const bint& x) const
-    {
+    bint operator+(const bint& x) const{
         bint ans;
         ans.l = l > x.l ? l : x.l;
-        for (int i = 0; i < ans.l; i++)
-        {
+        for (int i = 0; i < ans.l; i++){
             ans.w[i] += w[i] + x.w[i];
             ans.w[i + 1] += ans.w[i] / 10;
             ans.w[i] = ans.w[i] % 10;
@@ -28,15 +23,12 @@ struct bint
         if (ans.w[ans.l] != 0) ans.l++;
         return ans;
     }
-    bint operator*(const bint& x) const
-    {
+    bint operator*(const bint& x) const{
         bint res;
         int up, tmp;
-        for (int i = 0; i < l; i++)
-        {
+        for (int i = 0; i < l; i++){
             up = 0;
-            for (int j = 0; j < x.l; j++)
-            {
+            for (int j = 0; j < x.l; j++){
                 tmp = w[i] * x.w[j] + res.w[i + j] + up;
                 res.w[i + j] = tmp % 10;
                 up = tmp / 10;
@@ -47,8 +39,7 @@ struct bint
         while (res.w[res.l - 1] == 0 && res.l > 1) res.l--;
         return res;
     }
-    void print()
-    {
+    void print(){
         for (int i = l - 1; ~i; i--) printf("%d", w[i]);
         puts("");
     }
