6bfb2a3926608bc6f40231556e81553c5a357eed
superkun <superkunn@users.noreply.github.com>
2018-08-06 08:37:30 +0800

    Update 03_Manacher.cpp

--- 02_String Processing/03_Manacher.cpp
+++ 02_String Processing/03_Manacher.cpp
@@ -1,26 +1,38 @@
-// ---
-// O(n)求解最长回文子串
-// ---
-const int N = "Edit";
-char s[N], str[N << 1];
-int p[N << 1];
-void Manacher(char s[], int& n)
-{
-    str[0] = '$', str[1] = '#';
-    for (int i = 0; i < n; i++) str[(i << 1) + 2] = s[i], str[(i << 1) + 3] = '#';
-    n = 2 * n + 2;
-    str[n] = 0;
-    int mx = 0, id;
-    for (int i = 1; i < n; i++)
-    {
-        p[i] = mx > i ? min(p[2 * id - i], mx - i) : 1;
-        while (str[i - p[i]] == str[i + p[i]]) p[i]++;
-        if (p[i] + i > mx) mx = p[i] + i, id = i;
+const int MAXN=3e5;//more than two times
+char s[MAXN],str[MAXN];
+int len1,len2,p[MAXN];
+void init(){
+    str[0]='$';
+    str[1]='#';
+    rep(i,0,len1-1){
+        str[i*2+2]=s[i];
+        str[i*2+3]='#';
+    }
+    len2=len1*2+2;
+    str[len2]='*';
+}
+void manacher(){
+    int id=0,mx=0;
+    rep(i,1,len2-1){
+        if(mx>i)p[i]=min(p[2*id-i],mx-i);
+        else p[i]=1;
+        while(str[i+p[i]]==str[i-p[i]])p[i]++;
+        if(p[i]+i>mx){
+            mx=p[i]+i;
+            id=i;
+        }
     }
 }
-int solve(char s[])
-{
-    int n = strlen(s);
-    Manacher(s, n);
-    return *max_elememt(p, p + n) - 1;
+int work(){
+    while(scanf("%s",s)!=EOF){
+        len1=strlen(s);
+        init();
+        manacher();
+        int ans=0;
+        rep(i,0,len2-1){
+            ans=max(ans,p[i]);
+        }
+        printf("%d\n",ans-1);
+    }
+    return 0;
 }
