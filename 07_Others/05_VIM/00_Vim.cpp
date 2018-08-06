b252ccd65535990c221bcfd8bf86094f0ac5a2fa
superkun <superkunn@users.noreply.github.com>
2018-08-06 21:16:18 +0800

    Update 00_Vim.cpp

--- 07_Others/05_VIM/00_Vim.cpp
+++ 07_Others/05_VIM/00_Vim.cpp
@@ -1,36 +1,51 @@
 syntax on
-set cindent
 set nu
 set tabstop=4
 set shiftwidth=4
-set background=dark
+set cin
 set mouse=a
 
-map<C-A> ggvG"+y
-map<F5> :call Run()<CR>
-
-func! Run()
-    exec "w"
-    exec "!g++ -std=c++11 -O2 % -o %<"
-    exec "!time ./%<"
-endfunc
-
-autocmd BufNewFile *.cpp 0r ~/include.cpp
-autocmd BufNewFile *.cpp normal G
-
-inoremap ( ()<Esc>i
-inoremap [ []<Esc>i
-inoremap { {<CR>}<Esc>O
-inoremap ' ''<Esc>i
-inoremap " ""<Esc>i
-
-inoremap ) <c-r>=ClosePair(')')<CR>
-inoremap ] <c-r>=ClosePair(']')<CR>
-
-func ClosePair(char)
-	if getline('.')[col('.')-1]==a:char
-		return "\<Right>"
-	else
-		return a:char
-	endif
+map<F2> :call SetTitle()<CR>
+map<F3> :call setline(1,'')<CR>
+func SetTitle()
+let l = 0
+let l = l + 1 | call setline(l,'#define superkunn')
+let l = l + 1 | call setline(l,'#include <algorithm>')
+let l = l + 1 | call setline(l,'#include  <iostream>')
+let l = l + 1 | call setline(l,'#include   <cstring>')
+let l = l + 1 | call setline(l,'#include    <string>')
+let l = l + 1 | call setline(l,'#include    <cstdio>')
+let l = l + 1 | call setline(l,'#include    <vector>')
+let l = l + 1 | call setline(l,'#include    <cstdio>')
+let l = l + 1 | call setline(l,'#include    <vector>')
+let l = l + 1 | call setline(l,'#include     <stack>')
+let l = l + 1 | call setline(l,'#include     <queue>')
+let l = l + 1 | call setline(l,'#include     <cmath>')
+let l = l + 1 | call setline(l,'#include       <set>')
+let l = l + 1 | call setline(l,'#include       <map>')
+let l = l + 1 | call setline(l,'using namespace std;')
+let l = l + 1 | call setline(l,'#define rep(i,a,b) for(int i=a;i<=b;i++)')
+let l = l + 1 | call setline(l,'#define per(i,a,b) for(int i=a;i>=b;i--)')
+let l = l + 1 | call setline(l,'#define pb push_back')
+let l = l + 1 | call setline(l,'#define mp make_pair')
+let l = l + 1 | call setline(l,'#define all(x) (x).begin(),(x).end()')
+let l = l + 1 | call setline(l,'#define fi first')
+let l = l + 1 | call setline(l,'#define se second')
+let l = l + 1 | call setline(l,'#define SZ(x) ((int)(x).size())')
+let l = l + 1 | call setline(l,'typedef unsigned long long ull;')
+let l = l + 1 | call setline(l,'typedef long long ll;')
+let l = l + 1 | call setline(l,'typedef vector<int> vi;')
+let l = l + 1 | call setline(l,'typedef pair<int,int> pii;')
+let l = l + 1 | call setline(l,'/*************head******************/')
+let l = l + 1 | call setline(l,'int work(){')
+let l = l + 1 | call setline(l,'')
+let l = l + 1 | call setline(l,'    return 0;')
+let l = l + 1 | call setline(l,'}')
+let l = l + 1 | call setline(l,'int main(){')
+let l = l + 1 | call setline(l,'#ifdef superkunn')
+let l = l + 1 | call setline(l,'    freopen("input.txt","rt",stdin);')
+let l = l + 1 | call setline(l,'#endif')
+let l = l + 1 | call setline(l,'    work();')
+let l = l + 1 | call setline(l,'    return 0;')
+let l = l + 1 | call setline(l,'}')
 endfunc
