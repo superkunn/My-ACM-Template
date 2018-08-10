315edd7a759f9f53232fb8ee6459297bb1ce3199
superkun <superkunn@users.noreply.github.com>
2018-08-10 15:20:31 +0800

    Update 01_poj3468.cpp

--- 03_Data Structure/01_Binary Indexed Tree/01_poj3468.cpp
+++ 03_Data Structure/01_Binary Indexed Tree/01_poj3468.cpp
@@ -1,4 +1,4 @@
-// $a_{i}=\sum_{i=1}^{x}d_{i}$\\
+// a_{i}=\sum_{i=1}^{x}d_{i} \\
 // $\sum_{i=1}^{x}a_{i}=\sum_{i=1}^{x}\sum_{j=1}^{i}d_{j}=\sum_{i=1}^{x}(x-i+1)d_{i}$\\
 // $\sum_{i=1}^{x}a_{i}=(x+1)\sum_{i=1}^{x}d_{i}-\sum_{i=1}^{x}d_{i}\times&space;i$\\
 const int MAXN=1e5+5;
