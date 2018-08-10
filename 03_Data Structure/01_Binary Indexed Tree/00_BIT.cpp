34b16d07ee7baa4087294e084bb0eab2a0ae7349
superkun <superkunn@users.noreply.github.com>
2018-08-10 14:51:28 +0800

    Update 00_BIT.cpp

--- 03_Data Structure/01_Binary Indexed Tree/00_BIT.cpp
+++ 03_Data Structure/01_Binary Indexed Tree/00_BIT.cpp
@@ -1,7 +1,5 @@
-// ---
-// $O(\log n)$查询和修改数组的前缀和
-// ---
-// 注意下标应从1开始
+//add(pos,a) sum(r)-sum(l-1)
+//add(l,a) add(r+1,-a) sum(pos)
 const int MAXN=100000;
 struct BIT{
     int n,c[MAXN<<1];
@@ -9,3 +7,5 @@ struct BIT{
     void add(int i,int v){for(;i<=n;i+=i&-i)c[i]+=v;}
     int sum(int i){int s=0;for(;i>0;i-=i&-i)s+=c[i];return s;}
 }bit;
+
+
