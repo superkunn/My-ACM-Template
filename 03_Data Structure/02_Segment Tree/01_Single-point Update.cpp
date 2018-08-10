5b63050c43fdf28ce2a7aef7a10dc633815fdf69
superkun <superkunn@users.noreply.github.com>
2018-08-10 15:35:43 +0800

    Update 01_Single-point Update.cpp

--- 03_Data Structure/02_Segment Tree/01_Single-point Update.cpp
+++ 03_Data Structure/02_Segment Tree/01_Single-point Update.cpp
@@ -1,37 +1,35 @@
-const int maxn = "Edit";
-int sum[maxn << 2]; // sum[rt]用于维护区间和
-void PushUp(int rt) { sum[rt] = sum[lson] + sum[rson]; }
-void build(int l, int r, int rt)
-{
-    if (l == r)
-    {
-        scanf("%d", &sum[rt]); // 建立的时候直接输入叶节点
+const int MAXN=5e4+5;
+int sum[MAXN<<2];
+void push_up(int rt){
+    sum[rt]=sum[lson]+sum[rson];
+}
+void build(int l,int r,int rt){
+    if(l==r){
+        scanf("%d",&sum[rt]);
         return;
     }
-    int m = (l + r) >> 1;
-    build(Lson);
-    build(Rson);
-    PushUp(rt);
+    int mid;
+    build(le);
+    build(ri);
+    push_up(rt);
 }
-void update(int p, int add, int l, int r, int rt)
-{
-    if (l == r)
-    {
-        sum[rt] += add;
+void update(int p,int v,int l,int r,int rt){
+    if(l==r){
+        sum[rt]+=v;
         return;
     }
-    int m = (l + r) >> 1;
-    if (p <= m)
-        update(p, add, Lson);
-    else
-        update(p, add, Rson);
-    PushUp(rt);
+    int mid;
+    if(p<=m)update(p,v,le);
+    else update(p,v,ri);
+    push_up(rt);
 }
-int query(int L, int R, int l, int r, int rt)
-{
-    if (L <= l && r <= R) return sum[rt];
-    int m = (l + r) >> 1, s = 0;
-    if (L <= m) s += query(L, R, Lson);
-    if (m < R) s += query(L, R, Rson);
-    return s;
+int query(int L,int R,int l,int r,int rt){
+    if(L<=l&&r<=R){
+        return sum[rt];
+    }
+    int mid;
+    int ret=0;
+    if(L<=m)ret+=query(L,R,le);
+    if(R>m)ret+=query(L,R,ri);
+    return ret;
 }
