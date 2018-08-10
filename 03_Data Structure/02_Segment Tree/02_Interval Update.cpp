f1fb06014041c679e057c82bf3c48854fbbc68c0
superkun <superkunn@users.noreply.github.com>
2018-08-10 17:27:52 +0800

    Update 02_Interval Update.cpp

--- 03_Data Structure/02_Segment Tree/02_Interval Update.cpp
+++ 03_Data Structure/02_Segment Tree/02_Interval Update.cpp
@@ -1,48 +1,50 @@
-const int maxn = "Edit";
-int seg[maxn << 2], sum[maxn << 2]; // seg[rt]用于存放懒惰标记，注意PushDown时标记的传递
-void PushUp(int rt) { sum[rt] = sum[lson] + sum[rson]; }
-void PushDown(int rt, int m)
-{
-    if (seg[rt] == 0) return;
-    seg[lson] += seg[rt];
-    seg[rson] += seg[rt];
-    sum[lson] += seg[rt] * (m - (m >> 1));
-    sum[rson] += seg[rt] * (m >> 1);
-    seg[rt] = 0;
+const int MAXN=1e5+5;
+ll lazy[MAXN<<2];
+ll tree[MAXN<<2];
+void push_up(int rt){
+    tree[rt]=tree[lson]+tree[rson];
 }
-void build(int l, int r, int rt)
-{
-    seg[rt] = 0;
-    if (l == r)
-    {
-        scanf("%lld", &sum[rt]);
+void push_down(int rt,int m){
+    ll w=lazy[rt];
+    if(w){
+        lazy[lson]+=w;
+        lazy[rson]+=w;
+        tree[lson]+=w*(m-(m>>1));
+        tree[rson]+=w*(m>>1);
+        lazy[rt]=0;
+    }
+}
+void build(int l,int r,int rt){
+    lazy[rt]=0;
+    if(l==r){
+        scanf("%lld",&tree[rt]);
         return;
     }
-    int m = (l + r) >> 1;
-    build(Lson);
-    build(Rson);
-    PushUp(rt);
+    int mid;
+    build(le);
+    build(ri);
+    push_up(rt);
 }
-void update(int L, int R, int add, int l, int r, int rt)
-{
-    if (L <= l && r <= R)
-    {
-        seg[rt] += add;
-        sum[rt] += add * (r - l + 1);
+void update(int L,int R,int v,int l,int r,int rt){
+    if(L<=l&&r<=R){
+        lazy[rt]+=v;
+        tree[rt]+=1ll*v*(r-l+1);
         return;
     }
-    PushDown(rt, r - l + 1);
-    int m = (l + r) >> 1;
-    if (L <= m) update(L, R, add, Lson);
-    if (m < R) update(L, R, add, Rson);
-    PushUp(rt);
+    push_down(rt,r-l+1);
+    int mid;
+    if(L<=m)update(L,R,v,le);
+    if(R>m)update(L,R,v,ri);
+    push_up(rt);
 }
-int query(int L, int R, int l, int r, int rt)
-{
-    if (L <= l && r <= R) return sum[rt];
-    PushDown(rt, r - l + 1);
-    int m = (l + r) >> 1, ret = 0;
-    if (L <= m) ret += query(L, R, Lson);
-    if (m < R) ret += query(L, R, Rson);
+ll query(int L,int R,int l,int r,int rt){
+    if(L<=l&&r<=R){
+        return tree[rt];
+    }
+    push_down(rt,r-l+1);
+    int mid;
+    ll ret=0;
+    if(L<=m)ret+=query(L,R,le);
+    if(R>m)ret+=query(L,R,ri);
     return ret;
 }
