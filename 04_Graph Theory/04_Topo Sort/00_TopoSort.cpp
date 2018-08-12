c0db4146c2b5b1fd229ca0c0d2e740e68b385fac
superkun <superkunn@users.noreply.github.com>
2018-08-12 09:26:52 +0800

    Update 00_TopoSort.cpp

--- 04_Graph Theory/04_Topo Sort/00_TopoSort.cpp
+++ 04_Graph Theory/04_Topo Sort/00_TopoSort.cpp
@@ -1,38 +1,71 @@
-// ---
-// 存图前记得初始化\\
-// Ans排序结果，G邻接表，deg入度，map用于判断重边\\
-// 排序成功返回1，存在环返回0
-// ---
-const int maxn = "Edit";
-int Ans[maxn];
-vector<int> G[maxn];
-int deg[maxn];
-map<PII, bool> S;
-void init(int n)
-{
-    S.clear();
-    for (int i = 0; i < n; i++) G[i].clear();
-    clr(deg, 0), clr(Ans, 0);
+//cf 915D
+const int MAXN=505;
+const int MAXM=1e5+5;
+int n,m;
+int tot;
+int head[MAXN],cur[MAXN],idec[MAXN];
+struct Edge{
+    int v,nxt;
+    Edge(){}
+    Edge(int v,int nxt):v(v),nxt(nxt){}
+}edge[MAXM];
+void init(){
+    tot=0;
+    clr(head,-1);
 }
-void add_edge(int u, int v)
-{
-    if (S[mp(u, v)]) return;
-    G[u].pb(v), S[mp(u, v)] = 1, deg[v]++;
+void add_edge(int u,int v){
+    edge[tot]=Edge(v,head[u]);
+    head[u]=tot++;
 }
-bool Toposort(int n)
-{
-    int tot = 0;
-    queue<int> q;
-    for (int i = 0; i < n; ++i)
-        if (deg[i] == 0) q.push(i);
-    while (!q.empty())
-    {
-        int u = q.front();
-        que.pop();
-        Ans[tot++] = u;
-        for (auto& v : G[u])
-            if (--deg[v] == 0) q.push(t);
+int que[MAXN];
+int st,ed;
+bool topsort(int x){
+    int nst=1,ned=0;
+    rep(i,1,n)cur[i]=idec[i];
+    cur[x]--;
+    que[++ned]=x;
+    while(nst<=ned){
+        int u=que[nst++];
+        for(int i=head[u];i!=-1;i=edge[i].nxt){
+            int v=edge[i].v;
+            if(--cur[v]==0)que[++ned]=v;
+        }
     }
-    if (tot < n - 1) return false;
-    return true;
+    if(ned+ed==n)return true;
+    else return false;
+}
+int work(){
+    scanf("%d%d",&n,&m);
+    init();
+    while(m--){
+        int u,v;
+        scanf("%d%d",&u,&v);
+        add_edge(u,v);
+        idec[v]++;
+    }
+    st=1,ed=0;
+    rep(i,1,n){
+        if(idec[i]==0)que[++ed]=i;
+    }
+    while(st<=ed){
+        int u=que[st++];
+        for(int i=head[u];i!=-1;i=edge[i].nxt){
+            int v=edge[i].v;
+            if(--idec[v]==0)que[++ed]=v;
+        }
+    }
+    if(ed==n){
+        puts("YES");
+        return 0;
+    }
+    rep(i,1,n){
+        if(idec[i]==1){
+            if(topsort(i)){
+                puts("YES");
+                return 0;
+            }
+        }
+    }
+    puts("NO");
+    return 0;
 }
