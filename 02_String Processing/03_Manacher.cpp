5da099013bf13ac6d6dbbd7cfe384aa802320dd7
superkun <superkunn@users.noreply.github.com>
2018-08-16 09:06:48 +0800

    Update 03_Manacher.cpp

--- 02_String Processing/03_Manacher.cpp
+++ 02_String Processing/03_Manacher.cpp
@@ -1,38 +1,39 @@
-const int MAXN=3e5;//more than two times
+//hihocoder 1032
+const int MAXN=2e6+10;//more than 2 times !
 char s[MAXN],str[MAXN];
 int len1,len2,p[MAXN];
 void init(){
     str[0]='$';
     str[1]='#';
-    rep(i,0,len1-1){
+    rep(i,0,len1){
         str[i*2+2]=s[i];
         str[i*2+3]='#';
     }
     len2=len1*2+2;
     str[len2]='*';
 }
-void manacher(){
-    int id=0,mx=0;
+int manacher(){
+    int id=0,mx=0,ans=0;
     rep(i,1,len2-1){
         if(mx>i)p[i]=min(p[2*id-i],mx-i);
         else p[i]=1;
         while(str[i+p[i]]==str[i-p[i]])p[i]++;
-        if(p[i]+i>mx){
-            mx=p[i]+i;
+        if(i+p[i]>mx){
+            mx=i+p[i];
             id=i;
         }
+        ans=max(ans,p[i]);
     }
+    return ans-1;
 }
 int work(){
-    while(scanf("%s",s)!=EOF){
+    int T;
+    scanf("%d",&T);
+    while(T--){
+        scanf("%s",s);
         len1=strlen(s);
         init();
-        manacher();
-        int ans=0;
-        rep(i,0,len2-1){
-            ans=max(ans,p[i]);
-        }
-        printf("%d\n",ans-1);
+        printf("%d\n",manacher());
     }
     return 0;
 }
