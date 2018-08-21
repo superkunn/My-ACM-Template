c8035fd24520c5f45b059f979c518b3f3c24068c
superkun <superkunn@users.noreply.github.com>
2018-08-21 10:28:58 +0800

    Create 03_Kosaraju.cpp

---
+++ 04_Graph Theory/06_Depth-First Traversal/03_Kosaraju.cpp
@@ -0,0 +1,47 @@
+const int MAXV=1e4+10;
+const int MAXE=1e5+10;
+int V,tot,scc;
+int head[MAXV],rhead[MAXV],Belong[MAXV];
+bool vis[MAXV];
+int stk[MAXV],top;
+struct Edge{
+    int v,nxt;
+    Edge(){}
+    Edge(int v,int nxt):v(v),nxt(nxt){}
+}edge[MAXE],redge[MAXE];
+void init(int _n){
+    V=_n;
+    tot=scc=top=0;
+    clr(head,-1);
+    clr(rhead,-1);
+}
+void add_edge(int u,int v){
+    edge[tot]=Edge(v,head[u]);
+    redge[tot]=Edge(u,rhead[v]);
+    head[u]=rhead[v]=tot++;
+}
+void dfs(int u){
+    vis[u]=true;
+    for(int i=head[u];~i;i=edge[i].nxt){
+        int v=edge[i].v;
+        if(!vis[v])dfs(v);
+    }
+    stk[++top]=u;
+}
+void rdfs(int u,int k){
+    vis[u]=true;
+    Belong[u]=k;
+    for(int i=rhead[u];~i;i=redge[i].nxt){
+        int v=redge[i].v;
+        if(!vis[v])rdfs(v,k);
+    }
+}
+void kscc(){
+    clr(vis,0);
+    rep(i,1,V)if(!vis[i])dfs(i);
+    clr(vis,0);
+    per(i,top,1){
+        int v=stk[i];
+        if(!vis[v])rdfs(v,++scc);
+    }
+}
