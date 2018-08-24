cd8b50ed4d5ef3500d68381e7c5b5e4ef4c24c69
superkun <superkunn@users.noreply.github.com>
2018-08-24 20:01:32 +0800

    Update 00_Fast Power.cpp

--- 01_Math/06_Fast Power/00_Fast Power.cpp
+++ 01_Math/06_Fast Power/00_Fast Power.cpp
@@ -1,11 +1,3 @@
-ll pow_mod(ll a,ll b,ll mod) {
-    ll res=1;
-    for(;b;b>>=1){
-        if(b&1)res=res*a%mod;
-        a=a*a%mod;
-    }
-    return res;
-}
 //hdu 5187
 ll mul_mod(ll a,ll b,ll mod){
     ll res=0;
@@ -15,3 +7,21 @@ ll mul_mod(ll a,ll b,ll mod){
     }
     return res;
 }
+ll pow_mod(ll a,ll b,ll mod) {
+    ll res=1;
+    for(;b;b>>=1){
+        if(b&1)res=mul_mod(res,a,mod);
+        a=mul_mod(a,a,mod);
+    }
+    return res;
+}
+/*****************/
+ll pow_mod(ll a,ll b,ll mod) {
+    ll res=1;
+    for(;b;b>>=1){
+        if(b&1)res=res*a%mod;
+        a=a*a%mod;
+    }
+    return res;
+}
+
