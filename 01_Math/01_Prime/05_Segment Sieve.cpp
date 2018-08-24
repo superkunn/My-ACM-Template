8216f7820da9d0b1e63ef9fb922bd117660c655d
superkun <superkunn@users.noreply.github.com>
2018-08-24 21:34:32 +0800

    Update 05_Segment Sieve.cpp

--- 01_Math/01_Prime/05_Segment Sieve.cpp
+++ 01_Math/01_Prime/05_Segment Sieve.cpp
@@ -1,28 +1,24 @@
-// ---
-// 对区间$[a,b)$内的整数执行筛法。\\
-// 函数返回区间内素数个数\\
-// is\_prime[i-a]=true表示i是素数\\
-// $a<b \le 10^{12}, b-a \le 10^6$
-// ---
-const int maxn = "Edit";
-bool is_prime_small[maxn], is_prime[maxn];
-int prime[maxn];
-int segment_sieve(ll a, ll b)
-{
-    int tot = 0;
-    for (ll i = 0; i * i < b; ++i)
-        is_prime_small[i] = true;
-    for (ll i = 0; i < b - a; ++i)
-        is_prime[i] = true;
-    for (ll i = 2; i * i < b; ++i)
-        if (is_prime_small[i])
-        {
-            for (ll j = 2 * i; j * j < b; j += i)
-                is_prime_small[j] = false;
-            for (ll j = max(2LL, (a + i - 1) / i) * i; j < b; j += i)
-                is_prime[j - a] = false;
+const int MAXN=1e6+5;
+//[a,b)
+bool is_prime[MAXN];
+bool is_prime_small[MAXN];
+ll prime[MAXN];//1 base
+int segment_sieve(ll a,ll b){
+    int cnt=0;
+    for(int i=0;1LL*i*i<b;i++)is_prime_small[i]=true;
+    is_prime_small[0]=is_prime_small[1]=false;
+    for(int i=0;i<b-a;i++)is_prime[i]=true;
+    if(a==1)is_prime[0]=false;
+
+    for(int i=2;1LL*i*i<b;i++){
+        if(is_prime_small[i]){
+            for(int j=2*i;1LL*j*j<b;j+=i)is_prime_small[j]=false;//[2,sqrt(b))
+            for(ll j=max(2LL,(a+i-1)/i)*i;j<b;j+=i)is_prime[j-a]=false;
         }
-    for (ll i = 0; i < b - a; ++i)
-        if (is_prime[i]) prime[tot++] = i + a;
-    return tot;
+    }
+    //[a,b) [0,b-a)
+    for(ll i=0;i<b-a;i++){
+        if(is_prime[i])prime[++cnt]=i+a;
+    }
+    return cnt;
 }
