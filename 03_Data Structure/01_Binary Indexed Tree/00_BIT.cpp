60dd6007cdae05d4eca91d16a2d98f9013cddb71
superkun <superkunn@users.noreply.github.com>
2018-08-27 08:44:16 +0800

    Update 00_BIT.cpp

--- 03_Data Structure/01_Binary Indexed Tree/00_BIT.cpp
+++ 03_Data Structure/01_Binary Indexed Tree/00_BIT.cpp
@@ -8,4 +8,32 @@ struct BIT{
     int sum(int i){int s=0;for(;i>0;i-=i&-i)s+=c[i];return s;}
 }bit;
 
+struct BIT{
+    int n,c[MAXN<<1];
+    void init(int _n){
+        n=_n;
+        rep(i,0,n)c[i]=0;
+    }
+    void update(int i,int v){
+        for(;i<=n;i+=i&-i)c[i]+=v;
+    }
+    int query(int i){
+        int s=0;
+        for(;i;i-=i&-i)s+=c[i];
+        return s;
+    }
+    int findpos(int v){
+        int sum=0;
+        int pos=0;
+        int i=1;
+        for(;i<n;i<<=1);
+        for(;i;i>>=1){
+            if(pos+i<=n&&sum+c[pos+i]<v){
+                sum+=c[pos+i];
+                pos+=i;
+            }
+        }
+        return pos+1;
+    }
+}bit;
 
