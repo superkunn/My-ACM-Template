7020555ceb549465f2bf917e304f6f61296ee5b4
superkun <superkunn@users.noreply.github.com>
2018-08-27 08:57:14 +0800

    Update 00_Union-Find-Set.cpp

--- 04_Graph Theory/01_Union-Find Set/00_Union-Find-Set.cpp
+++ 04_Graph Theory/01_Union-Find Set/00_Union-Find-Set.cpp
@@ -1,7 +1,7 @@
 const int MAXN=1e6+5;
 struct DSU{
     int p[MAXN];
-    void init(int n){for(int i=0;i<=n;i++)p[i]=i;}
+    void init(int n){rep(i,0,n)p[i]=i;}
     int findp(int x){return x==p[x]?x:p[x]=findp(p[x]);}
     void unite(int x,int y){x=findp(x);y=findp(y);if(x==y)return;p[y]=x;}
     bool same(int x,int y){return findp(x)==findp(y);}
