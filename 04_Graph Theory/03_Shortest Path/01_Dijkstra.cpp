db026c797edcc8ff9f4255386b26d91db6b50ef6
superkun <superkunn@users.noreply.github.com>
2018-09-01 20:58:27 +0800

    Update 01_Dijkstra.cpp

--- 04_Graph Theory/03_Shortest Path/01_Dijkstra.cpp
+++ 04_Graph Theory/03_Shortest Path/01_Dijkstra.cpp
@@ -1,66 +1,64 @@
-//cf 610 A
 #include<bits/stdc++.h>
 using namespace std;
-const int INF=1e9;
-const int MAXV=5e3+50;
-const int MAXE=1e5+50;
-int V;
-struct edge{int to,cost;};
-vector<edge> G[MAXV];
-typedef pair<int,int> P;
-int d[MAXV];
-void dijkstra(int s){
+#define rep(i,a,b) for(int i=a;i<=b;i++)
+#define clr(a,x) memset(a,x,sizeof(a))
+#define mp make_pair
+const int MAXV=2e6;
+const int MAXE=5e6+10;
+typedef long long anytype;
+typedef pair<anytype,int> P;
+int tot=0;
+int head[MAXV];
+struct Edge{
+    int v,c,nxt;
+    Edge(){}
+    Edge(int v,int c,int nxt):v(v),c(c),nxt(nxt){}
+}edge[MAXE];
+void init(){
+    tot=0;
+    clr(head,-1);
+}
+void add_edge(int u,int v,int c){
+    edge[tot]=Edge(v,c,head[u]);
+    head[u]=tot++;
+}
+anytype d[MAXV];
+void dij(int s){
     priority_queue<P,vector<P>,greater<P> > que;
-    fill(d,d+V+1,INF);
+    clr(d,-1);
     d[s]=0;
     que.push(P(0,s));
     while(!que.empty()){
         P t=que.top();
         que.pop();
         int v=t.second;
-        if(d[v]<t.first)continue;
-        for(int i=0;i<G[v].size();i++){
-            edge e=G[v][i];
-            if(d[e.to]>d[v]+e.cost){
-                d[e.to]=d[v]+e.cost;
-                que.push(P(d[e.to],e.to));
+        if(d[v]!=-1&&d[v]<t.first)continue;
+        for(int i=head[v];~i;i=edge[i].nxt){
+            Edge e=edge[i];
+            if(d[e.v]==-1||d[e.v]>d[v]+e.c){
+                d[e.v]=d[v]+e.c;
+                que.push(P(d[e.v],e.v));
             }
         }
     }
 }
-int mat[405][405];
 int main(){
-    int n,m;
-    scanf("%d%d",&n,&m);
-    V=n;
-    for(int i=1;i<=m;i++){
-        int u,v;
-        scanf("%d%d",&u,&v);
-        G[u].push_back(edge{v,1});
-        G[v].push_back(edge{u,1});
-        mat[u][v]=mat[v][u]=1;
-    }
-    dijkstra(1);
-    int ans;
-    if(d[n]==INF){
-        printf("-1");
-        return 0;
-    }
-    ans=d[n];
-    for(int i=1;i<=n;i++)G[i].clear();
-    for(int i=1;i<=n;i++){
-        for(int j=1;j<=n;j++){
-            if(i==j)continue;
-            if(mat[i][j]==0){
-                G[i].push_back(edge{j,1});
+    int T;
+    scanf("%d",&T);
+    while(T--){
+        int n,m,k;
+        scanf("%d%d%d",&n,&m,&k);
+        init();
+        rep(i,1,m){
+            int u,v,c;
+            scanf("%d%d%d",&u,&v,&c);
+            rep(j,0,k){
+                add_edge(u+j*n,v+j*n,c);
+                if(j!=k)add_edge(u+j*n,v+(j+1)*n,0);
             }
         }
+        dij(1);
+        printf("%lld\n",d[n+k*n]);
     }
-    dijkstra(1);
-    if(d[n]==INF){
-        printf("-1");
-        return 0;
-    }
-    printf("%d",max(ans,d[n]));
     return 0;
 }
