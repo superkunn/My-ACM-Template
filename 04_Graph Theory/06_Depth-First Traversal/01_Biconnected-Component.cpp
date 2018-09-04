8686cb400a147df5f3250d3d1ab8c7533beaacef
superkun <superkunn@users.noreply.github.com>
2018-09-04 13:50:27 +0800

    Update 01_Biconnected-Component.cpp

--- 04_Graph Theory/06_Depth-First Traversal/01_Biconnected-Component.cpp
+++ 04_Graph Theory/06_Depth-First Traversal/01_Biconnected-Component.cpp
@@ -1,58 +1,131 @@
-//割顶的bccno无意义
-const int maxn = "Edit";
-int pre[maxn], iscut[maxn], bccno[maxn], dfs_clock, bcc_cnt;
-vector<int> G[maxn], bcc[maxn];
-stack<PII> s;
-void init(int n)
-{
-    for (int i = 0; i < n; i++) G[i].clear();
+//UVALive - 3523
+#include<bits/stdc++.h>
+using namespace std;
+#define clr(a,x) memset(a,x,sizeof(a))
+#define rep(i,a,b) for(int i=a;i<=b;i++)
+#define mp make_pair
+#define fi first
+#define se second
+#define pb push_back
+typedef pair<int,int> pii;
+typedef vector<int> vi;
+const int MAXV=1e3+10;
+const int MAXE=1e6+10;
+int tot;
+int head[MAXV];
+struct Edge{
+    int v,nxt;
+    Edge(){}
+    Edge(int v,int nxt):v(v),nxt(nxt){}
+}edge[MAXE<<1];
+void init(){
+    tot=0;
+    clr(head,-1);
 }
-inline void add_edge(int u, int v) { G[u].pb(v), G[v].pb(u); }
-int dfs(int u, int fa)
-{
-    int lowu = pre[u] = ++dfs_clock;
-    int child = 0;
-    for (auto& v : G[u])
-    {
-        PII e = mp(u, v);
-        if (!pre[v])
-        {
-            //没有访问过v
-            s.push(e);
+void add_edge(int u,int v){
+    edge[tot]=Edge(v,head[u]);
+    head[u]=tot++;
+}
+int pre[MAXV],is_cut[MAXV],bccno[MAXV],dfs_clock,bcc_cnt;
+vi bcc[MAXV];
+stack<pii > st;
+int dfs(int u,int fa){
+    int lowu=pre[u]=++dfs_clock;
+    int child=0;
+    for(int i=head[u];~i;i=edge[i].nxt){
+        int v=edge[i].v;
+        pii e=mp(u,v);
+        if(!pre[v]){
+            st.push(e);
             child++;
-            int lowv = dfs(v, u);
-            lowu = min(lowu, lowv); //用后代的low函数更新自己
-            if (lowv >= pre[u])
-            {
-                iscut[u] = true;
+            int lowv=dfs(v,u);
+            lowu=min(lowu,lowv);
+            if(lowv>=pre[u]){
+                is_cut[u]=1;
                 bcc_cnt++;
-                bcc[bcc_cnt].clear(); //注意！bcc从1开始编号
-                for (;;)
-                {
-                    PII x = s.top();
-                    s.pop();
-                    if (bccno[x.X] != bcc_cnt)
-                        bcc[bcc_cnt].pb(x.X), bcc[x.X] = bcc_cnt;
-                    if (bccno[x.Y] != bcc_cnt)
-                        bcc[bcc_cnt].pb(x.Y), bcc[x.Y] = bcc_cnt;
-                    if (x.X == u && x.Y == v) break;
+                bcc[bcc_cnt].clear();
+                for(;;){
+                    pii x=st.top();
+                    st.pop();
+                    if(bccno[x.fi]!=bcc_cnt){
+                        bcc[bcc_cnt].pb(x.fi);
+                        bccno[x.fi]=bcc_cnt;
+                    }
+                    if(bccno[x.se]!=bcc_cnt){
+                        bcc[bcc_cnt].pb(x.se);
+                        bccno[x.se]=bcc_cnt;
+                    }
+                    if(x.fi==u&&x.se==v)break;
+                }
+            }
+        }else if(pre[v]<pre[u]&&v!=fa){
+            st.push(e);
+            lowu=min(lowu,pre[v]);
+        }
+    }
+    if(fa<0&&child==1)is_cut[u]=0;
+    return lowu;
+}
+void find_bcc(int n){
+    clr(pre,0);
+    clr(is_cut,0);
+    clr(bccno,0);
+    dfs_clock=bcc_cnt=0;
+    rep(i,1,n){
+        if(!pre[i])dfs(i,-1);
+    }
+}
+int odd[MAXV],color[MAXV];
+bool bipartite(int u,int b){
+    for(int i=head[u];~i;i=edge[i].nxt){
+        int v=edge[i].v;
+        if(bccno[v]!=b)continue;
+        if(color[v]==color[u])return false;
+        if(!color[v]){
+            color[v]=3-color[u];
+            if(!bipartite(v,b))return false;
+        }
+    }
+    return true;
+}
+bool mmp[MAXV][MAXV];
+int main(){
+    int n,m;
+    while(scanf("%d%d",&n,&m),n+m){
+        clr(mmp,0);
+        rep(i,1,m){
+            int x,y;
+            scanf("%d%d",&x,&y);
+            mmp[x][y]=1;
+            mmp[y][x]=1;
+        }
+        init();
+        rep(i,1,n){
+            rep(j,i+1,n){
+                if(!mmp[i][j]){
+                    add_edge(i,j);
+                    add_edge(j,i);
                 }
             }
         }
-        else if (pre[v] < pre[u] && v != fa)
-        {
-            s.push(e);
-            lowu = min(lowu, pre[v]); //用反向边更新自己
+        find_bcc(n);
+        clr(odd,0);
+        for(int i=1;i<=bcc_cnt;i++){
+            clr(color,0);
+            for(int j=0;j<bcc[i].size();j++){
+                bccno[bcc[i][j]]=i;
+            }
+            int u=bcc[i][0];
+            color[u]=1;
+            if(!bipartite(u,i)){
+                for(int j=0;j<bcc[i].size();j++){
+                    odd[bcc[i][j]]=1;
+                }
+            }
         }
+        int ans=n;
+        rep(i,1,n)if(odd[i])ans--;
+        printf("%d\n",ans);
     }
-    if (fa < 0 && child == 1) iscut[u] = 0;
-    return lowu;
-}
-void find_bcc(int n)
-{
-    //调用结束后s保证为空，所以不用清空
-    clr(pre, 0), clr(iscut, 0), clr(bccno, 0);
-    dfs_clock = bcc_cnt = 0;
-    for (int i = 0; i < n; i++)
-        if (!pre[i]) dfs(i, -1);
+    return 0;
 }
