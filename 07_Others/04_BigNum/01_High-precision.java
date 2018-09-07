ae907ee73af57b22cd10c9efdb1012efe468382d
superkun <superkunn@users.noreply.github.com>
2018-09-07 14:54:01 +0800

    Update and rename 01_High-precision.cpp to 01_High-precision.java

---
+++ 07_Others/04_BigNum/01_High-precision.java
@@ -0,0 +1,22 @@
+
+import java.io.*;
+import java.math.BigInteger;
+  
+public class Main {
+    public static void main(String args[]) throws IOException {
+        StreamTokenizer in = new StreamTokenizer(new BufferedReader(new InputStreamReader(System.in)));
+        PrintWriter out = new PrintWriter(new OutputStreamWriter(System.out));
+ 
+        BigInteger a;
+        BigInteger b;
+        in.nextToken();
+        int A=(int) in.nval;
+        in.nextToken();
+        int B=(int) in.nval;
+ 
+        a=BigInteger.valueOf(A);
+        b=BigInteger.valueOf(B);
+        out.println(a.pow(B).subtract(b.pow(A)));
+        out.flush();
+    }
+}
