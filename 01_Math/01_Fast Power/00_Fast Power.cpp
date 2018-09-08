51d00cb29a800a4c482880d538cc03679c368038
superkun <superkunn@users.noreply.github.com>
2018-09-08 10:01:47 +0800

    Create 00_Fast Power.cpp

---
+++ 01_Math/01_Fast Power/00_Fast Power.cpp
@@ -0,0 +1,25 @@
+typedef long long ll;
+ll mul_mod(ll a,ll b,ll mod){
+    ll res=0;
+    for(;b;b>>=1){
+        if(b&1)res=(res+a)%mod;
+        a=(a<<1)%mod;
+    }
+    return res;
+}
+ll pow_mod(ll a,ll b,ll mod){//a^b
+    ll res=1;
+    for(;b;b>>=1){
+        if(b&1)res=mul_mod(res,a,mod)%mod;
+        a=mul_mod(a,a,mod)%mod;
+    }
+    return res;
+}
+ll pow_mod(ll a,ll b,ll mod){//a^b
+    ll res=1;
+    for(;b;b>>=1){
+        if(b&1)res=res*a%mod;
+        a=a*a%mod;
+    }
+    return res;
+}
