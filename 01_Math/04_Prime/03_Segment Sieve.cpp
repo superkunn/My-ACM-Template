89410992b83b6c3fcf587a5831c6b9f47f47fb95
superkun <superkunn@users.noreply.github.com>
2018-09-08 11:14:44 +0800

    Create 03_Segment Sieve.cpp

---
+++ 01_Math/04_Prime/03_Segment Sieve.cpp
@@ -0,0 +1,23 @@
+const int MAXN=1e6+5;	
+//[a,b)	
+bool is_prime[MAXN];	
+bool is_prime_small[MAXN];	
+ll prime[MAXN];//1 base	
+int segment_sieve(ll a,ll b){	
+    int cnt=0;	
+    for(int i=0;1LL*i*i<b;i++)is_prime_small[i]=true;	
+    is_prime_small[0]=is_prime_small[1]=false;	
+    for(int i=0;i<b-a;i++)is_prime[i]=true;	
+    if(a==1)is_prime[0]=false;	
+     for(int i=2;1LL*i*i<b;i++){	
+        if(is_prime_small[i]){	
+            for(int j=2*i;1LL*j*j<b;j+=i)is_prime_small[j]=false;//[2,sqrt(b))	
+            for(ll j=max(2LL,(a+i-1)/i)*i;j<b;j+=i)is_prime[j-a]=false;	
+        }	
+    }	
+    //[a,b) [0,b-a)	
+    for(ll i=0;i<b-a;i++){	
+        if(is_prime[i])prime[++cnt]=i+a;	
+    }	
+    return cnt;	
+}
