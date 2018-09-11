ac6334142fd6f9d286fb6c1e2ce7f6d7ff6f8721
superkun <superkunn@users.noreply.github.com>
2018-09-11 18:36:20 +0800

    Update 01_Eular.cpp

--- 01_Math/02_Eular phi/01_Eular.cpp
+++ 01_Math/02_Eular phi/01_Eular.cpp
@@ -3,13 +3,10 @@ using namespace std;
 typedef long long ll;
 const int MAXN=10000;
 int phi[MAXN];
-int phi1(int n)
-{
+int phi1(int n){
     int res=n;
-    for(int i=2;i*i<=n;i++)
-    {
-        if(n%i==0)
-        {
+    for(int i=2;i*i<=n;i++){
+        if(n%i==0){
             res=res/i*(i-1);
             for(;n%i==0;n/=i);
         }
@@ -17,8 +14,7 @@ int phi1(int n)
     if(n!=1) res=res/n*(n-1);
     return res;
 }
-void phi2(int n)
-{
+void phi2(int n){
     for(int i=0;i<=n;i++) phi[i]=i;
     for(int i=2;i<=n;i++)
         if(phi[i]==i)
