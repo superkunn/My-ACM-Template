875c009dd10e561b0881b4554419a766c77d6bed
superkun <superkunn@users.noreply.github.com>
2018-10-02 20:12:40 +0800

    Update 07_HashString.cpp

--- 02_String Processing/07_HashString.cpp
+++ 02_String Processing/07_HashString.cpp
@@ -1,26 +1,61 @@
-const ll B1=1e7+7;
-const ll B2=1e9+7;
-char pa[10004];
-char tx[1000006];
-int work(){
-    int T;
-    scanf("%d",&T);
-    while(T--){
-        scanf("%s%s",pa,tx);
-        int pl=strlen(pa);
-        int tl=strlen(tx);
-        ll w=1;
-        rep(i,1,pl)w=(w*B1)%B2;
-        ll ph=0,th=0;
-        rep(i,0,pl-1){
-            ph=(ph*B1+pa[i])%B2;
-            th=(th*B1+tx[i])%B2;
+//poj 3974
+#include<cstdio>
+#include<algorithm>
+#include<cstring>
+using namespace std;
+typedef unsigned long long ull;
+const int MAXN=1e6+10;
+char s[MAXN];
+ull a[MAXN];
+ull b[MAXN];
+ull base[MAXN];
+inline ull H(int i, int j) {
+	return (a[j] - a[i - 1] * base[j - i + 1]);
+}
+inline ull H2(int i, int j) {
+	return (b[i] - b[j + 1] * base[j - i + 1]);
+}
+int main(){
+    base[0]=1;
+    for(int i=1;i<MAXN;i++){
+        base[i]=base[i-1]*131;
+    }
+    int kase=0;
+    for(;;){
+        scanf("%s",s+1);
+        if(s[1]=='E')break;
+        int len=strlen(s+1);
+        a[0]=b[len+1]=0;
+        for(int i=1;i<=len;i++){
+            a[i]=a[i-1]*131+s[i]-'a';
         }
-        int ans=0;
-        for(int i=0;i+pl<=tl;i++){
-            if(ph==th)ans++;
-            if(i+pl<tl)th=(th*B1+tx[i+pl]-tx[i]*w)%B2;
+        for(int i=len;i>=1;i--){
+            b[i]=b[i+1]*131+s[i]-'a';
         }
+        int ans=1;
+        for(int pos=1;pos<=len;pos++){
+            int l=1,r=min(pos-1,len-pos);
+            while(l<=r){
+                int mid=(l+r)>>1;
+                if(H(pos-mid,pos-1)==H2(pos+1,pos+mid)){
+                    ans=max(2*mid+1,ans);
+                    l=mid+1;
+                }else{
+                    r=mid-1;
+                }
+            }
+            l=1,r=min(pos-1,len-pos+1);
+            while(l<=r){
+                int mid=(l+r)>>1;
+                if(H(pos-mid,pos-1)==H2(pos,pos+mid-1)){
+                    ans=max(2*mid,ans);
+                    l=mid+1;
+                }else{
+                    r=mid-1;
+                }
+            }
+        }
+        printf("Case %d: ",++kase);
         printf("%d\n",ans);
     }
     return 0;
