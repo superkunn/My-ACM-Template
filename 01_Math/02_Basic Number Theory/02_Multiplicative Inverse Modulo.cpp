e6204771002c68f3395a1d73769c255b192f6225
superkun <superkunn@users.noreply.github.com>
2018-10-02 11:15:58 +0800

    Update 02_Multiplicative Inverse Modulo.cpp

--- 01_Math/02_Basic Number Theory/02_Multiplicative Inverse Modulo.cpp
+++ 01_Math/02_Basic Number Theory/02_Multiplicative Inverse Modulo.cpp
@@ -11,3 +11,27 @@ inv[1]=1;
 for(int i=2;i<=40;i++){
     inv[i]=(p-(p/i))*inv[p%i]%p;
 }
+//fact invfact
+int fact[MAXN];
+int invfact[MAXN];
+ll pow_mod(ll a,ll b){
+    ll res=1;
+    while(b){
+        if(b&1)res=res*a%MOD;
+        a=a*a%MOD;
+        b>>=1;
+    }
+    return res;
+}
+ll fun(ll n,ll m){
+    return (1LL*fact[n]*invfact[m])%MOD*invfact[n-m]%MOD;
+}
+int n=100000;
+fact[0]=1;
+for(int i=1;i<=n;i++){
+    fact[i]=1LL*fact[i-1]*i%MOD;
+}
+invfact[n]=pow_mod(fact[n],MOD-2);
+for(int i=n;i>=1;i--){
+    invfact[i-1]=1LL*invfact[i]*i%MOD;
+}
