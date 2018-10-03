8b3c4b50f121698ebd6409aeb734e2158ab55477
superkun <superkunn@users.noreply.github.com>
2018-10-03 09:25:04 +0800

    Create 05_SA.cpp

---
+++ 02_String Processing/05_SA.cpp
@@ -0,0 +1,51 @@
+#include<iostream>
+#include<cstdio>
+#include<cstring>
+#include<algorithm>
+#include<vector>
+using namespace std;
+const int SIZE = 300010, INF = 1 << 30;
+int a[SIZE], sa[SIZE], rk[SIZE], fir[SIZE], sec[SIZE], c[SIZE], h[SIZE];
+char str[SIZE];
+int len;
+bool comp(int i, int j, int k){
+	return sec[i] == sec[j] && sec[i + k] == sec[j + k];
+}
+void sufarr(int n){
+	int i, p, l, m = 200;
+	for (i = 0;i < m;i++) c[i] = 0;
+	for (i = 0;i < n;i++) c[rk[i] = a[i]]++;
+	for (i = 1;i < m;i++) c[i] += c[i - 1];
+	for (i = n - 1;i >= 0;i--) sa[--c[a[i]]] = i;
+	for (l = p = 1;p < n;l *= 2, m = p)
+	{
+		for (p = 0, i = n - l;i < n;i++) sec[p++] = i;
+		for (i = 0;i < n;i++)
+			if (sa[i] >= l) sec[p++] = sa[i] - l;
+		for (i = 0;i < n;i++) fir[i] = rk[sec[i]];
+		for (i = 0;i < m;i++) c[i] = 0;
+		for (i = 0;i < n;i++) c[fir[i]]++;
+		for (i = 1;i < m;i++) c[i] += c[i - 1];
+		for (i = n - 1;i >= 0;i--) sa[--c[fir[i]]] = sec[i];
+		memcpy(sec, rk, sizeof(rk));
+		rk[sa[0]] = 0;
+		for (i = p = 1;i < n;i++)
+			rk[sa[i]] = comp(sa[i], sa[i - 1], l) ? p - 1 : p++;
+	}
+}
+void calh(){
+	int i, j, k = 0;
+	for (i = 1;i <= len;i++) rk[sa[i]] = i;
+	for (i = 0;i < len;h[rk[i++]] = k)
+		for (k ? k-- : 0, j = sa[rk[i] - 1];a[i + k] == a[j + k];k++);
+}
+int main(){
+	scanf("%s", str);
+	len = strlen(str);
+	for (int i = 0;i < len;i++) a[i] = str[i];
+	a[len] = 0;
+	sufarr(len + 1);
+	calh();
+	for(int i = 1; i <= len; i++) printf("%d ", sa[i]); puts("");
+	for(int i = 1; i <= len; i++) printf("%d ", h[i]); puts("");
+}
