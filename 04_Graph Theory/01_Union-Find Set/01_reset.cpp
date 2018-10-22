21d5347733fa8b3d76e7eb0096267c3d7199ed6c
superkun <superkunn@users.noreply.github.com>
2018-10-22 21:55:28 +0800

    Update 01_reset.cpp

--- 04_Graph Theory/01_Union-Find Set/01_reset.cpp
+++ 04_Graph Theory/01_Union-Find Set/01_reset.cpp
@@ -24,3 +24,38 @@ struct DSU{
     }
     bool same(int x,int y){return findp(x)==findp(y);}
 }dsu;
+
+//带撤销的并查集
+namespace DSU2 {
+  const static int MAXN = 100000 + 10;
+  int fa[MAXN], ds[MAXN], rk[MAXN];
+  int S[MAXN], top;
+  void init(int n) {
+    for (int i = 1; i <= n; ++ i) {
+      fa[i] = i, rk[i] = ds[i] = 0;
+    }
+    top = 0;
+  }
+  int dis(int x) {
+    int r(0);
+    for (; x != fa[x]; x = fa[x]) r ^= ds[x];
+    return r;
+  }
+  int get(int x) {
+    while (x != fa[x]) x = fa[x];
+    return fa[x];
+  }
+  void merge(int x, int y, int d) {
+    x = get(x); y = get(y);
+    if (x == y) return;
+    if (rk[x] > rk[y]) std::swap(x, y);
+    if (rk[x] == rk[y]) ++ rk[y], S[++ top] = -y;
+    fa[x] = y; ds[x] = d; S[++ top] = x;
+  }
+  void restore(int ed) {
+    for (; top > ed; -- top) {
+      if (S[top] < 0) -- rk[-S[top]];
+      else fa[S[top]] = S[top], ds[S[top]] = 0;
+    }
+  }
+}
