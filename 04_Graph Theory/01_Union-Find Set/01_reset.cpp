ff11a7fa79dec8471fb0243834d1e93e1c3de450
superkun <superkunn@users.noreply.github.com>
2018-10-23 12:23:06 +0800

    Update 01_reset.cpp

--- 04_Graph Theory/01_Union-Find Set/01_reset.cpp
+++ 04_Graph Theory/01_Union-Find Set/01_reset.cpp
@@ -1,24 +1,21 @@
 struct DSU{
-    int p[MAXN],sz[MAXN];
-    int Back[MAXN][2];
+    int p[MAXN],rk[MAXN];
+    int Back[MAXN<<1];
     int cnt;
-    void init(int n){rep(i,0,n)p[i]=i,sz[i]=1;cnt=0;}
+    void init(int n){rep(i,0,n)p[i]=i,rk[i]=1;cnt=0;}
     int findp(int x){return x==p[x]?x:findp(p[x]);}
     void unite(int x,int y){
         x=findp(x);y=findp(y);if(x==y)return;
-        if(sz[x]>sz[y])swap(x,y);
+        if(rk[x]>rk[y])swap(x,y);
+        if(rk[x]==rk[y])++rk[y],Back[++cnt]=-y;
         p[x]=y;
-        sz[y]+=sz[x];
-        Back[++cnt][0]=x;
-        Back[++cnt][1]=y;
+        Back[++cnt]=x;
     }
     void save(){cnt=0;}
     void Cancel(){
         while(cnt){
-            int x=Back[cnt][0];
-            int y=Back[cnt][1];
-            p[x]=x;
-            sz[y]-=sz[x];
+            if(Back[cnt]<0)--rk[-Back[cnt]];
+            else p[Back[cnt]]=Back[cnt];
             cnt--;
         }
     }
