a5b4377968e2bab332ba464f2d7b8b82ea413615
superkun <superkunn@users.noreply.github.com>
2018-11-29 03:11:10 +0800

    Update 01_Dinic.cpp

--- 04_Graph Theory/09_Network Flow/01_Dinic.cpp
+++ 04_Graph Theory/09_Network Flow/01_Dinic.cpp
@@ -1,42 +1,61 @@
-//poj 3281
-#include<cstdio>
-#include<iostream>
-#include<algorithm>
-#include<cstring>
-#include<queue>
+#include <algorithm>
+#include  <iostream>
+#include   <cstring>
+#include    <string>
+#include    <cstdio>
+#include    <vector>
+#include    <bitset>
+#include     <stack>
+#include     <queue>
+#include     <cmath>
+#include       <set>
+#include       <map>
 using namespace std;
+#define rep(i,a,b) for(int i=a;i<=b;i++)
+#define per(i,a,b) for(int i=a;i>=b;i--)
 #define clr(a,x) memset(a,x,sizeof(a))
-const int MAXV=400+5;
+#define pb push_back
+#define all(x) (x).begin(),(x).end()
+#define fi first
+#define se second
+#define mp make_pair
+#define SZ(x) ((int)(x).size())
+typedef unsigned long long ull;
+typedef long long ll;
+typedef vector<int> vi;
+typedef pair<int,int> pii;
+/*************head******************/
+const int MAXV=1e4+10;
 const int MAXE=1e5+5;
-const int INF=0x3f3f3f3f;
-int tot;
-int head[MAXV],level[MAXV],iter[MAXV];
+const ll INF=0x3f3f3f3f3f3f3f3f;
+ll tot;
+ll head[MAXV],level[MAXV],iter[MAXV];
 struct Edge{
-    int v,cap,nxt;
+    ll v,cap,nxt;
     Edge(){}
-    Edge(int v,int cap,int nxt):v(v),cap(cap),nxt(nxt){}
+    Edge(ll v,ll cap,ll nxt):v(v),cap(cap),nxt(nxt){}
 }edge[MAXE<<1];
 void init(){
     tot=0;
     clr(head,-1);
 }
-void add_edge(int u,int v,int c){
+void add_edge(ll u,ll v,ll c){
     edge[tot]=Edge(v,c,head[u]);
     head[u]=tot++;
     edge[tot]=Edge(u,0,head[v]);
     head[v]=tot++;
 }
-void bfs(int s){
+void bfs(ll s){
     clr(level,-1);
     level[s]=0;
-    queue<int> que;
+    queue<ll> que;
     que.push(s);
     while(!que.empty()){
-        int u=que.front();
+        ll u=que.front();
         que.pop();
-        for(int i=head[u];~i;i=edge[i].nxt){
-            int v=edge[i].v;
-            int c=edge[i].cap;
+        for(ll i=head[u];~i;i=edge[i].nxt){
+            ll v=edge[i].v;
+            ll c=edge[i].cap;
             if(c>0&&level[v]<0){
                 level[v]=level[u]+1;
                 que.push(v);
@@ -44,13 +63,13 @@ void bfs(int s){
         }
     }
 }
-int dfs(int u,int t,int f){
+ll dfs(ll u,ll t,ll f){
     if(u==t)return f;
-    for(int &i=iter[u];~i;i=edge[i].nxt){
-        int v=edge[i].v;
-        int c=edge[i].cap;
+    for(ll &i=iter[u];~i;i=edge[i].nxt){
+        ll v=edge[i].v;
+        ll c=edge[i].cap;
         if(c>0&&level[u]<level[v]){
-            int d=dfs(v,t,min(f,c));
+            ll d=dfs(v,t,min(f,c));
             if(d>0){
                 edge[i].cap-=d;
                 edge[i^1].cap+=d;
@@ -60,42 +79,35 @@ int dfs(int u,int t,int f){
     }
     return 0;
 }
-int max_flow(int s,int t){
-    int flow=0;
+ll max_flow(ll s,ll t){
+    ll flow=0;
     while(1){
         bfs(s);
         if(level[t]<0)return flow;
-        int f;
+        ll f;
         memcpy(iter,head,sizeof(head));
         while(f=dfs(s,t,INF))flow+=f;
     }
 }
 int main(){
-    int n,f,d;
-    scanf("%d%d%d",&n,&f,&d);
-    int s=0,t=2*n+f+d;
+    int n,m;
+    scanf("%d%d",&n,&m);
+    int s=0,t=n+m+1;
     init();
-    for(int i=1;i<=f;i++){
-        add_edge(s,2*n+i,1);
+    rep(i,1,n){
+        int x;
+        scanf("%d",&x);
+        add_edge(i,t,x);
     }
-    for(int i=1;i<=d;i++){
-        add_edge(2*n+f+i,t,1);
+    ll sum=0;
+    rep(i,1+n,n+m){
+        int x,y,z;
+        scanf("%d%d%d",&x,&y,&z);
+        add_edge(s,i,z);
+        add_edge(i,x,INF);
+        add_edge(i,y,INF);
+        sum+=z;
     }
-    for(int i=1;i<=n;i++){
-        add_edge(i,n+i,1);
-        int ff,dd;
-        scanf("%d%d",&ff,&dd);
-        while(ff--){
-            int x;
-            scanf("%d",&x);
-            add_edge(2*n+x,i,1);
-        }
-        while(dd--){
-            int x;
-            scanf("%d",&x);
-            add_edge(n+i,2*n+f+x,1);
-        }
-    }
-    printf("%d",max_flow(s,t));
+    printf("%I64d",sum-max_flow(s,t));
     return 0;
 }
